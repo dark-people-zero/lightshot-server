d73910ca3571f9f8c971fd27b33a1f613b045ede
Dark People <dark.people.zero@gmail.com>
2025-11-20 00:57:25 +0700

    update

--- index.js
+++ index.js
@@ -1,59 +1,50 @@
-const fs         = require('fs');
-const http       = require("http");
-const express    = require('express');
-const fileUpload = require('express-fileupload');
-const app        = express();
-const { uid }    = require('uid');
-const config     = require('./config.json');
+const fs = require("fs");
+const http = require("http");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const app = express();
+const { uid } = require("uid");
+const config = require("./config.json");
 
-if(!fs.existsSync(config.uploadDir)) fs.mkdirSync(config.uploadDir);
+if (!fs.existsSync(config.uploadDir)) fs.mkdirSync(config.uploadDir);
 
 app.use(fileUpload());
 
-app.use('/i', express.static(config.uploadDir))
+app.use("/i", express.static(config.uploadDir));
 
-app.post('/upload/:a/:b/', function(req, res) {
+app.post("/upload/:a/:b/", function (req, res) {
+	if (!req.files) return res.status(500).send("No file uploaded");
 
-  if (!req.files)
-    return res.status(500).send('No file uploaded');
+	let newFileName;
 
-  let newFileName;
+	for (let k in req.files) {
+		((file) => {
+			if (file.name.indexOf("thumb") == -1) {
+				const split = file.name.split(".");
+				const ext = split.pop();
+				fileName = uid(8) + "." + ext;
+				let count = 1;
 
-  for(let k in req.files) {
-      ((file) => {
+				while (fs.existsSync(config.uploadDir + "/" + fileName))
+					fileName = `${uid(8)}.${ext})`;
 
-        if(file.name.indexOf('thumb') == -1) {
+				file.mv(config.uploadDir + "/" + fileName, function (err) {
+					if (err) return res.status(500).send(err);
+				});
 
-          const split = file.name.split('.');
-          const ext   = split.pop();
-          fileName    = uid(8) + '.' + ext;
-          let count   = 1;
+				newFileName = fileName;
+			}
+		})(req.files[k]);
+	}
 
-          while(fs.existsSync(config.uploadDir + '/' + fileName))
-            fileName = `${uid(8)}.${ext})`;
-
-          file.mv(config.uploadDir + '/' + fileName, function(err) {
-            if (err)
-              return res.status(500).send(err);
-          });
-
-          newFileName = fileName;
-        }
-
-      })(req.files[k]);
-  }
-
-  const url  = config.baseUrl.replace('{{filename}}', newFileName);
-  const body = `<response><status>success</status><share>${url}</share></response>
-`
-  res.send(body);
+	const url = config.baseUrl.replace("{{filename}}", newFileName);
+	const body = `<response><status>success</status><share>${url}</share></response>
+`;
+	res.send(body);
 });
 
-app.g
+app.g;
 
-http
-  .createServer(app)
-  .listen(config.listenPort, () => {
-    console.log('lightshot http server started on port ' + config.listenPort);
-  })
-;
+http.createServer(app).listen(config.listenPort, () => {
+	console.log("lightshot http server started on port " + config.listenPort);
+});
